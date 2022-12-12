35c8c8ce5f3fdd6c3c1dcfc1189671e0e58e2c6a
Martin Cooley <martin.cooley@protonmail.com>
2022-12-12 09:21:39 -0500

    added support for two different numbers APIs

--- powerball.py
+++ powerball.py
@@ -1,9 +1,8 @@
-import os
-from gmail import Gmail
-from datetime import datetime, timedelta
-import time
-import urllib3
 import json
+import re
+import urllib3
+
+from gmail import Gmail
 
 myNumbers = [
     [ {10, 19, 29, 37, 46 }, {  6 } ],
@@ -63,31 +62,75 @@ def checkNumbers(currentNumbers: list, outputMessage: str):
     return outputMessage
 
 
-def getNumbers(outputMessage):
-    url = "https://data.ny.gov/api/views/d6yy-54nr/rows.json"
+def getNumbers(outputMessage: str, url: str):
+    """ This function supports parsing the data from two different API
+    endpoints.  There are very few differences between the two and those
+    differences are handled here.
+
+    Args:
+        outputMessage (str): This is the string to which the parsed JSON
+        data is appended.
+
+        url (str): This is the URL from which the data is requested.
+
+    Returns:
+        This returns two values:
+            An array of sets representing the winning numbers and powerball
+            Also returns the append string with formatted data.
+    """
+
+    dateField = ['field_draw_date', 'draw_date']
+    numbersField = ['field_winning_numbers', 'winning_numbers']
+
     http = urllib3.PoolManager()
     r = http.request('GET', url)
     jsonData = json.loads(r.data)
-    numbers = jsonData['data']
-    latestNumbers = numbers[-1:][0][9].split(' ')
+
+    lastDrawing = jsonData[0]
+    for df in dateField:
+        if df in lastDrawing:
+            dateValue = lastDrawing[df]
+
+    for nf in numbersField:
+        if nf in lastDrawing:
+            latestNumbers = lastDrawing[nf]
+
+
+    latestNumbers = re.split(' |,', latestNumbers)
+
     latestNumbers = [int(x) for x in latestNumbers]
-    retVal = [ set(latestNumbers[:-1])]
-    retVal.append(set(latestNumbers[-1:]))
+    powerball = latestNumbers.pop()
+
+    retVal = [ set(latestNumbers)]
+    pb = set()
+    pb.add(powerball)
+    retVal.append(pb)
+
+    outputMessage = "Current Date: {0}\n".format(dateValue)
     outputMessage = printEntry(outputMessage, "Current Numbers: {0} \t\tPowerball: {1}\n", retVal)
 
     return retVal, outputMessage
 
-def main(json_data, context):
-    currentNumbers, outputMessage = getNumbers(outputMessage = '')
+
+def main():
+
+    url1 = "https://data.ny.gov/resource/d6yy-54nr.json"
+    url2 = "https://www.powerball.com/api/v1/numbers/powerball/recent?_format=json"
+
+    currentNumbers, outputMessage = getNumbers(outputMessage = '', url = url1)
     outputMessage = checkNumbers(currentNumbers, outputMessage)
 
-    g = Gmail()
-    g.setFrom('martin.cooley@gmail.com')
-    g.addRecipient('martin.cooley@gmail.com')
-    g.subject("Do you want to be a milli?")
-    g.message(outputMessage)
-    username = os.environ['username']
-    appkey = os.environ["appkey"]
-    g.setAuth(username, appkey)
-    g.send()
+    # g = Gmail()
+    # g.setFrom('martin.cooley@gmail.com')
+    # g.addRecipient('martin.cooley@gmail.com')
+    # g.subject("Do you want to be a milli?")
+    # g.message(outputMessage)
+    # username = os.environ['username']
+    # appkey = os.environ["appkey"]
+    # g.setAuth(username, appkey)
+    # g.send()
 
+    print(outputMessage)
+
+
+main()
